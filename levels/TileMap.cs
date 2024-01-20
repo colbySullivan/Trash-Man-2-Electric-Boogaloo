b43c187c7c4159ab45988d386bbac2ee7f3c0b1b
Colby Sullivan <sullivanc@southwestern.edu>
2024-01-20 00:00:31 -0600

    random generated background script

--- levels/TileMap.cs
+++ levels/TileMap.cs
@@ -6,8 +6,8 @@ public partial class TileMap : Godot.TileMap
 	public FastNoiseLite moisture = new FastNoiseLite();
 	public FastNoiseLite temperature = new FastNoiseLite();
 	public FastNoiseLite altitude = new FastNoiseLite();
-	public const int width = 128;
-	public const int height = 128;
+	public int width = 128;
+	public int height = 128;
 	private CharacterBody2D player { get; set; }
 	
 	// Called when the node enters the scene tree for the first time.
@@ -23,8 +23,6 @@ public partial class TileMap : Godot.TileMap
 	public override void _Process(double delta)
 	{
 		this.player = this.GetNode<CharacterBody2D>("TrashMan");
-		GD.Print(player.Position);
-		
 		generate_chunk(player.Position);
 		
 	}
@@ -33,18 +31,11 @@ public partial class TileMap : Godot.TileMap
 	{
 		var tile_pos = LocalToMap(position);
 		for(int x = 0; x < width; x++){
-			for(int y = 0; x < height; x++){
+			for(int y = 0; y < height; y++){
 				var moist = moisture.GetNoise2D(tile_pos.X-width/2 + x, tile_pos.Y-height/2 + y)*10;
 				var temp = temperature.GetNoise2D(tile_pos.X-width/2 + x, tile_pos.Y-height/2 + y)*10;
 				var alt = altitude.GetNoise2D(tile_pos.X-width/2 + x, tile_pos.Y-height/2 + y)*10;
-				Vector2I vec; 
-				vec = new Vector2I((int)tile_pos.X-width/2 + x, (int)tile_pos.Y-height/2 + y);
-				Vector2I vec2;
-				vec2 = new Vector2I((int)Math.Round((moist+10)/5), (int)Math.Round((temp+10)/5));
-				// vec2 = new Vector2I(0,0);
-				// vec = new Vector2I(0,0);
-				SetCell(0, vec, 0, vec2);
-				
+				SetCell(0, new Vector2I((tile_pos.X-64)+x, (tile_pos.Y-64)+y), 0, new Vector2I((int)(moist+10)/5, (int)(temp+10)/5));
 			}
 		}
 	}
