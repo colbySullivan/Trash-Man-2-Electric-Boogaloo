c965dfd6dce8b4f70e8f6a54817b487629e9c1a6
Colby Sullivan <sullivanc@southwestern.edu>
2024-01-24 11:10:20 -0600

    Up and down walking scripts

--- scripts/TrashMan.cs
+++ scripts/TrashMan.cs
@@ -28,8 +28,8 @@ public partial class TrashMan : CharacterBody2D
 		// Handle trash wack
 		if (Input.IsActionPressed("fight"))
 			_animatedSprite.Play("fight");
-		else
-			_animatedSprite.Play("idle");
+		//else
+			//_animatedSprite.Play("idle");
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
@@ -45,13 +45,25 @@ public partial class TrashMan : CharacterBody2D
 			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
 		}
 		if(velocity.X < 0)
+		{
+			_animatedSprite.Play("idle");
 			_animatedSprite.FlipH = true;
+		}	
 		if(velocity.X > 0)
+		{
+			_animatedSprite.Play("idle");
 			_animatedSprite.FlipH = false;
+		}	
 		if(velocity.Y < 0)
-			_animatedSprite.FlipV = true;
+		{
+			_animatedSprite.Play("idleup");
+			//_animatedSprite.FlipV = true;
+		}
 		if(velocity.Y > 0)
-			_animatedSprite.FlipV = false;
+		{
+			_animatedSprite.Play("idledown");
+			//_animatedSprite.FlipV = false;
+		}	
 		Velocity = velocity;
 		MoveAndSlide();
 	}
