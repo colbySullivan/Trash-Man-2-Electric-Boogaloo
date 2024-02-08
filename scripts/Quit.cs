3df1cdab29039fb89e37937c4d04832d3caa12aa
Colby Sullivan <sullivanc@southwestern.edu>
2024-02-08 14:37:24 -0600

    quit button script documentation #6

--- scripts/Quit.cs
+++ scripts/Quit.cs
@@ -6,14 +6,19 @@ public partial class Quit : Button
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		// Initialization logic can be added here if needed
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		// Currently, no continuous processing logic in this method
 	}
+
+	// Called when the button is pressed
 	private void _on_pressed()
 	{
+		// Quits the game when the button is pressed
 		GetTree().Quit();
 	}
 }
