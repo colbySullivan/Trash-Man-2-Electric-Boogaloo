730769f739fc1a90a7e411cc80265e1494dd578e
Colby Sullivan <sullivanc@southwestern.edu>
2024-02-08 14:38:04 -0600

    start button script documentation #6

--- scripts/Start.cs
+++ scripts/Start.cs
@@ -6,14 +6,19 @@ public partial class Start : Button
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		// Initialization logic can be added here if needed
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		// Currently, no continuous processing logic in this method
 	}
+
+	// Called when the button is pressed
 	private void _on_pressed()
 	{
+		// Changes the scene to "level1.tscn" when the button is pressed
 		GetTree().ChangeSceneToFile("res://levels/level1.tscn");
 	}
 }
